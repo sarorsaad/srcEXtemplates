564ccea55f400507b9300efcc535ba755827330c
elsaad92 <99730108+elsaad92@users.noreply.github.com>
2023-05-02 06:17:58 +0300

    app templates added

--- doctor/urls.py
+++ doctor/urls.py
@@ -8,4 +8,11 @@ app_name='doctor'
 urlpatterns = [
     path('doctor/dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
     # other URL patterns...
+    path('booking/', views.booking, name='booking'),
+    
+    path('profile/', views.view_profile, name='doctor_profile'),
+    
+    path('reports/', views.reports, name='reports'),
+    
+     path('tests-library/', views.tests_library, name='tests_library'),
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 def doctor_dashboard(request):
     # Your view code here
     return render(request, 'doctor/doctor_dashboard.html', context={})
+
+
+from django.shortcuts import render
+
+def booking(request):
+    return render(request, 'doctor/Booking.html')
+
+
+def view_profile(request):
+    return render(request, 'doctor/Profile.html')
+
+
+
+def reports(request):
+    return render(request, 'doctor/Reports.html')
+
+
+def tests_library(request):
+    return render(request, 'doctor/Tests Library.html')
+
